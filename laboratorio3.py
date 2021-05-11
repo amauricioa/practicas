16f573c3b27c07dc432f8c25efdbbf01e3d0209f
Mauricio <amauricioa@yahoo.es>
2021-05-11 11:54:09 -0500

    Ejercicios semana2 modulo2

---
+++ laboratorio3.py
@@ -0,0 +1,6 @@
+# Coversor de divisas
+# Convertir de dolares a pesos
+valor_pesos = float(input("Ingresa la cantidad de pesos a convertir: "))
+tmr = 3714.94
+valor_usd = valor_pesos / tmr
+print(valor_pesos, "pesos equivale a,", valor_usd, "Dolares")
---
+++ laboratorio4.py
@@ -0,0 +1,6 @@
+# Calculo del IMC
+altura = 1.75
+peso = 85
+# Sigue el programa aqui
+imc = peso/altura**2
+print("Tu IMC (Indice de Masa Corporal) es:", imc)
